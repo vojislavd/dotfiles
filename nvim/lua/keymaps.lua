8ea411eca7d7876f802ce64b26ea4029f1c7ce60
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-14 22:57:00 +0100

    Remove mapleader and maplocalleader from keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,3 @@
-vim.g.mapleader=" "
-vim.g.maplocalleader=" "
-
 -- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
