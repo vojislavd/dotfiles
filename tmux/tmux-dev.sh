92e97243975894f97fdfe7c154b66ad7843f6a3f
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-14 08:30:49 +0100

    Rename tmux-dev-server to tmux-dev and add new window

--- tmux/tmux-dev.sh
+++ tmux/tmux-dev.sh
@@ -10,13 +10,13 @@ sudo systemctl start nginx
 sudo systemctl start mysql
 
 # Session name
-SESSION_NAME="server"
+SESSION_NAME="dev"
 
 # Kill session if already exists
 tmux kill-session -t $SESSION_NAME 2>/dev/null
 
 # Start a new detached tmux session
-tmux new-session -d -s $SESSION_NAME
+tmux new-session -d -s $SESSION_NAME -n "services"
 
 # Wait a bit to finish previous command before running next
 sleep 0.1
@@ -33,3 +33,13 @@ tmux send-keys -t $SESSION_NAME:1.1 "MINIO_ROOT_USER="$MINIO_USER" MINIO_ROOT_PA
 
 # Run Mailpit in the second pane
 tmux send-keys -t $SESSION_NAME:1.2 "/usr/local/bin/mailpit" C-m
+
+# Create new window
+tmux new-window -t $SESSION_NAME -n "workspace"
+
+# Switch to code directory
+tmux send-keys -t $SESSION_NAME:workspace "cd ~/code/ && clear" C-m
+
+# Attach to the session
+tmux attach -t $SESSION_NAME
+
