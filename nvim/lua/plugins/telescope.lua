d3bd42ee9ae007179996f30f0d555c39d3fadf28
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-15 23:17:44 +0100

    Move telescope keymaps from keymaps.lua to telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,15 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope.nvim', 
+    tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+
+    config = function()
+        local telescope = require('telescope')
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { silent = true, desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true, desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { silent = true, desc = 'Telescope buffer' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { silent = true, desc = 'Telescope help tags' })
+    end,
 }
