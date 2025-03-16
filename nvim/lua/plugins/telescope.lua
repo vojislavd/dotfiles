1dd17a9f7f869e9471051d1e5ddc16bad5bf5016
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-16 20:51:37 +0100

    Install telescope-file-browser plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,9 +7,20 @@ return {
         local telescope = require('telescope')
         local builtin = require('telescope.builtin')
 
+        telescope.setup {
+            extensions = {
+                file_browser = {
+                    hijeck_netrw = true, -- disable netrw and use telescope-file-browser in its place
+                },
+            },
+        }
+
+        telescope.load_extension "file_browser"
+
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { silent = true, desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true, desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { silent = true, desc = 'Telescope buffer' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { silent = true, desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fe', ':Telescope file_browser<CR>', { silent = true, desc = 'Browser project files' })
     end,
 }
