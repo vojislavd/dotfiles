12386f7b71984afb766fe8de16b3811cb6cc4d9d
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-21 21:56:06 +0100

    Install onedark plugin

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -26,6 +26,7 @@ return {
             auto_install = true,
             highlight = {
                 enable = true,
+                additional_vim_regex_highlighting = false,
             },
         }
     end,
