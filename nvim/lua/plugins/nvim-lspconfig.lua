8761a9831475f8d18d10911ef462745fccd2dfe8
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-22 18:05:57 +0100

    Remove phpactor config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,15 +4,7 @@ return {
         -- Setup your LSP configurations here
         local lspconfig = require('lspconfig')
 
-        lspconfig.phpactor.setup({
-            cmd = { 'phpactor', 'language-server' },
-            filetypes = { 'php' },
-            on_attach = function(client, bufnr)
-                require'cmp'.setup.buffer { sources = { { name = 'nvim_lsp' } } }
-
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-            end,
-        })
+        lspconfig.phpactor.setup({})
     end
 }
 
