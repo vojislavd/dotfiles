65faeec8b82fbadc4983b11724d0c3e5941473a8
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-23 09:37:45 +0100

    Add some keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,9 +10,19 @@ vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv==gv")
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', { noremap = true, silent = true })
 
--- Multiple cursors
-vim.g.VM_Mappings = {
-  ["<C-S-j>"] = "<C-n>",  -- Move cursor down (use <C-n> default behavior)
-  ["<C-S-k>"] = "<C-p>",  -- Move cursor up (use <C-p> default behavior)
-}
+-- Reselect visual selection after intendintg
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
+-- Keep the cursor position when yanking a visual selection
+vim.keymap.set('v', 'y', 'myy`y')
+
+-- Disable q:
+vim.keymap.set('n', 'q:', ':q')
+
+-- Paste replace visual selection without copying it
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Quickly insert of a trailing ; or , from insert mode
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
