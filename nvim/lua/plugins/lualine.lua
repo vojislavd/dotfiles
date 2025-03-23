e222e29661887cbc64c2600bb0b7a819248664ca
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-03-23 17:46:29 +0100

    Style lualine status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,6 +6,44 @@ return {
     config = function()
         local lualine = require('lualine')
 
-        lualine.setup({})
+        lualine.setup({
+            options = {
+                section_separators = '',
+                component_separators = '',
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = {
+                    'mode',
+                },
+                lualine_b = {
+                    'branch',
+                    {
+                        'diff',
+                        symbols = { added = ' ', modified = ' ', removed = ' ' }
+                    },
+                    function()
+                        return '󰅭 ' .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.buf_get_clients())) or '')
+                    end,
+                    {
+                        'diagnostics',
+                        sources = { 'nvim_diagnostic' },
+                    }
+                },
+                lualine_c = {
+                    'filename',
+                },
+                lualine_x = {},
+                lualine_y = {
+                    'filetype',
+                },
+                lualine_z = {
+                    'searchcount',
+                    'selectioncount',
+                    'location',
+                    'progress',
+                }
+            }
+        })
     end,
 }
