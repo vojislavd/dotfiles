23eeacfb2ea09615a018a8609cd87342fe17663c
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-10-19 09:55:47 +0200

    Use Chromium instead of Chrome

--- shortcuts.sh
+++ shortcuts.sh
@@ -29,12 +29,12 @@ dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/cus
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/name "'Open Brave browser incognito'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/binding "'<Alt>C'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/command "'google-chrome'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/name "'Open Chrome browser'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/command "'chromium'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/name "'Open Chromium browser'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/binding "'<Alt><Shift>C'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/command "'google-chrome --incognito'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/name "'Open Chrome browser incognito'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/command "'chromium --incognito'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/name "'Open Chromium browser incognito'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/binding "'<Alt>F'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/command "'firefox'"
@@ -45,12 +45,12 @@ dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/cus
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/name "'Open Firefox browser incognito'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/binding "'<Alt>A'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/command "'google-chrome https://chatgpt.com'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/name "'Open ChatGPT in Chrome browser'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/command "'chromium https://chatgpt.com'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/name "'Open ChatGPT in Chromium browser'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/binding "'<Alt><Shift>A'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/command "'google-chrome https://chatgpt.com/?temporary-chat=true'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/name "'Open ChatGPT temporary chat in Chrome browser'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/command "'chromium https://chatgpt.com/?temporary-chat=true'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/name "'Open ChatGPT temporary chat in Chromium browser'"
 
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/binding "'<Alt>S'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/command "'slack'"
