1e218888922827e16a7ee982517266a8faae8fc7
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-10-20 21:46:22 +0200

    Add vscode snippets

--- scripts/install-vscode-setup.sh
+++ scripts/install-vscode-setup.sh
@@ -2,6 +2,10 @@
 
 VSCODE_USER_DIR="$HOME/.config/Code/User"
 
+# Settings
 rm -rf "$VSCODE_USER_DIR/settings.json"
-
 ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_USER_DIR/settings.json"
+
+# Snippets
+rm -rf "$VSCODE_USER_DIR/snippets/php.json"
+ln -sf "$DOTFILES/vscode/snippets/php.json" "$VSCODE_USER_DIR/snippets/php.json"
