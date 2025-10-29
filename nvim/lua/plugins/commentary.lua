f7b661399f07d02066bf12f19ae85678edf26cc6
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-10-29 21:22:15 +0100

    Add configuration to commenting blade files

--- nvim/lua/plugins/commentary.lua
+++ nvim/lua/plugins/commentary.lua
@@ -1,4 +1,14 @@
 return {
 	'tpope/vim-commentary',
+
+    -- Configure comments for Blade files
+    config = function()
+        vim.api.nvim_create_autocmd("Filetype", {
+            pattern = "blade",
+            callback = function()
+                vim.opt_local.commentstring = "{{-- %s --}}"
+            end,
+        })
+    end,
 }
 
