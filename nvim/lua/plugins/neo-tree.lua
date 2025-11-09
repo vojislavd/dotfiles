1f437a773cba54bc6127783b9bda7339296b18c0
vojislavd <23532087+vojislavd@users.noreply.github.com>
2025-11-09 09:23:01 +0100

    Display hidden files in neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -15,7 +15,14 @@ return {
 	opts = {
         window = {
             position = 'right'
-        }
+        },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+        },
 	},
 }
 
