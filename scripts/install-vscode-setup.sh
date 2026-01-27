3e0daf78c5a410ceae3053b79ade4cdc79b3344f
vojislavd <23532087+vojislavd@users.noreply.github.com>
2026-01-27 12:37:55 +0100

    Add vscode keybindings configuration

--- scripts/install-vscode-setup.sh
+++ scripts/install-vscode-setup.sh
@@ -6,3 +6,6 @@ VSCODE_USER_DIR="$HOME/.config/Code/User"
 rm -rf "$VSCODE_USER_DIR/settings.json"
 ln -s "$DOTFILES/vscode/settings.json" "$VSCODE_USER_DIR/settings.json"
 
+# Keybindings
+ln -s "$DOTFILES/vscode/keybindings.json" "$VSCODE_USER_DIR/keybindings.json"
+
